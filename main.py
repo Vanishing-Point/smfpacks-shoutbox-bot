f22ac06e39ff220676ab1d47443a8a0a917d0a31
Vanishing-Point <osobieraj@gmail.com>
2016-05-06 22:34:10 +0200

    Added files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import network
+from models import MsgDatabase, DatabaseConnection
+from time import sleep
+      
+with network.Bot() as Malbolge, DatabaseConnection() as database:
+    while True:
+        for message in Malbolge.main():
+            database.updateUserRecord(message)
+            print message
+        for i in range(30):
+            sleep(0.1)
